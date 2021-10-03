821b86fa6b7a0d9bce8089d774a08eda485c3a60
Alec PC <27219575+Alecadabra@users.noreply.github.com>
2021-10-03 22:37:49 +0800

    Work on processing

--- Assignment/src/number_finder.py
+++ Assignment/src/number_finder.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 from matplotlib import pyplot as plt
+import random
 
 def showImage(img, name):
     cv.imshow(name, img)
@@ -21,6 +22,22 @@ def plotImage(img, name):
     plt.legend(('cdf', 'histogram'), loc = 'upper left')
     plt.show()
 
+def randomColour():
+    return (
+        random.randint(0, 0xff),
+        random.randint(0, 0xff),
+        random.randint(0, 0xff)
+    )
+
+def aspectRatio(contour):
+    x, y, width, height = cv.boundingRect(contour)
+
+    if height == 0:
+        # Divide by zero
+        return float('inf')
+    else:
+        return width / height
+
 def task1():
     sep = os.path.sep
     # Get the current project directory path
@@ -31,7 +48,7 @@ def task1():
     trainImgs = [cv.imread(f'{train}BS{imgNum:02d}.jpg') for imgNum in range(1, 22)]
 
     for (i, img) in enumerate(trainImgs, start=1):
-        #showImage(img, f'Image {i}')
+        # showImage(img, f'Image {i}')
 
         blurred = cv.GaussianBlur(img, (3, 3), 0)
 
@@ -39,9 +56,34 @@ def task1():
 
         edges = cv.Canny(greyBlurred, 300, 400)
 
-        showImage(edges, f'Canny of img {i}')
+        # showImage(edges, f'Canny of img {i}')
 
-        contours, hier = cv.findContours(edges, cv.CHAIN_APPROX_SIMPLE, )
+        _, contours, _ = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
-    
+        drawnContours = img.copy()
+
+        for i, _ in enumerate(contours):
+            drawnContours = cv.drawContours(drawnContours, contours, i, randomColour(), thickness=2)
+
+        # showImage(drawnContours, f'Contours for img {i}')
+
+        hulls = [cv.convexHull(contour) for contour in contours]
+
+        sortedHulls = sorted(hulls, key=lambda hull: aspectRatio(hull))[0:3]
+
+        sortedContours = sorted(contours, key=lambda contour: abs(aspectRatio(contour) - 2))[0:3]
+
+        drawnContours = img.copy()
+
+        for i, _ in enumerate(sortedHulls):
+            drawnContours = cv.drawContours(drawnContours, sortedContours, i, randomColour(), thickness=2)
+
+        drawnRects = img.copy()
+
+        for i, contour in enumerate(contours):
+            x, y, w, h = cv.boundingRect(contour)
+            drawnRects = cv.rectangle(drawnRects, (x, y), (x + w, y + h), color=randomColour(), thickness=2)
+        
+        showImage(drawnRects, f'Bounding rects for img {i}')
+
 
